1f51c0c63827a4b38dbc579a7d1c37ed4a18193f
abdullohsoatov0001 <maxidaxakimova@gmail.com>
2026-02-06 20:50:03 +0300

    torvour

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Nav from '../components/nav/Nav'
+import { Outlet } from 'react-router'
+import Footer from '../components/footer/Footer'
 
 const Layout = () => {
   return (
     <div>
-        
+      <Nav />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
